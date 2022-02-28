0244e31994da062fd469475a4e5913917c1d6b73
Yago Batista <yagobatistafis@yahoo.com.br>
2022-02-28 02:25:22 -0300

    spockcurrentgame design update

--- components/spock-game/SpockCurrentGame.js
+++ components/spock-game/SpockCurrentGame.js
@@ -14,110 +14,92 @@ const CurrentGame = () => {
   const selectionHandler = (option) => {
     spockDispatch({ type: "SUBMIT", value: option });
   };
-  const test = 400;
   return (
-    // <Flex>
-    //   <Button onClick={() => selectionHandler("ROCK")}>Rock</Button>
-    //   <Button onClick={() => selectionHandler("PAPER")}>Paper</Button>
-    //   <Button onClick={() => selectionHandler("SCISSORS")}>Scissors</Button>
-    //   <Button onClick={() => selectionHandler("LIZARD")}>Lizard</Button>
-    //   <Button onClick={() => selectionHandler("SPOCK")}>Spock</Button>
-    // </Flex>
     <Box
       h={["300px", "340px", "400px"]}
       w={["300px", "340px", "400px"]}
       position="relative"
     >
-      {/* <Box position="absolute" top="0" right="50%">
-        1
-      </Box> */}
       <PlayButton
         image={scissorsIcon}
         config={{
           borderColor: "yellow.500",
-          h: ["60px", "80px", "105px"],
-          w: ["60px", "80px", "105px"],
+          h: ["70px", "90px", "105px"],
+          w: ["70px", "90px", "105px"],
           borderWidth: ["7px", "10px"],
           top: "0",
-          left: ["calc(50% - 30px)", "calc(50% - 40px)", "calc(50% - 50.5px)"],
+          left: ["calc(50% - 35px)", "calc(50% - 45px)", "calc(50% - 50.5px)"],
           position: "absolute",
         }}
+        onClick={() => selectionHandler("SCISSORS")}
       />
       <PlayButton
-        image={rockIcon}
+        image={spockIcon}
         config={{
-          borderColor: "red.500",
-          h: ["60px", "80px", "105px"],
-          w: ["60px", "80px", "105px"],
+          borderColor: "blue.200",
+          h: ["70px", "90px", "105px"],
+          w: ["70px", "90px", "105px"],
           borderWidth: ["7px", "10px"],
           top: [
-            "calc(34.5% - 30px)",
-            "calc(34.5% - 40px)",
+            "calc(34.5% - 35px)",
+            "calc(34.5% - 45px)",
             "calc(34.5% - 50.5px)",
           ],
           left: "2.5%",
           position: "absolute",
         }}
+        onClick={() => selectionHandler("SPOCK")}
       />
-      {/* <Box position="absolute" top="34.5%" left="2.5%">
-         2
-       </Box> */}
-      {/* <Box position="absolute" top="34.5%" right="2.5%">
-        3
-      </Box> */}
       <PlayButton
-        image={spockIcon}
+        image={paperIcon}
         config={{
-          borderColor: "blue.200",
-          h: ["60px", "80px", "105px"],
-          w: ["60px", "80px", "105px"],
+          borderColor: "blue.600",
+          h: ["70px", "90px", "105px"],
+          w: ["70px", "90px", "105px"],
           borderWidth: ["7px", "10px"],
           top: [
-            "calc(34.5% - 30px)",
-            "calc(34.5% - 40px)",
+            "calc(34.5% - 35px)",
+            "calc(34.5% - 45px)",
             "calc(34.5% - 50.5px)",
           ],
           right: "2.5%",
           position: "absolute",
         }}
+        onClick={() => selectionHandler("PAPER")}
       />
-      {/* <Box position="absolute" top="90.5%" left="20.6%">
-        4
-      </Box> */}
       <PlayButton
-        image={rockIcon}
+        image={lizardIcon}
         config={{
-          borderColor: "red.500",
-          h: ["60px", "80px", "105px"],
-          w: ["60px", "80px", "105px"],
+          borderColor: "purple.500",
+          h: ["70px", "90px", "105px"],
+          w: ["70px", "90px", "105px"],
           borderWidth: ["7px", "10px"],
           top: [
-            "calc(90.5% - 60px)",
-            "calc(90.5% - 80px)",
+            "calc(90.5% - 70px)",
+            "calc(90.5% - 90px)",
             "calc(90.5% - 105px)",
           ],
           left: "20.6%",
           position: "absolute",
         }}
+        onClick={() => selectionHandler("LIZARD")}
       />
-      {/* <Box position="absolute" top="90.5%" right="20.6%">
-        5
-      </Box> */}
       <PlayButton
         image={rockIcon}
         config={{
           borderColor: "red.500",
-          h: ["60px", "80px", "105px"],
-          w: ["60px", "80px", "105px"],
+          h: ["70px", "90px", "105px"],
+          w: ["70px", "90px", "105px"],
           borderWidth: ["7px", "10px"],
           top: [
-            "calc(90.5% - 60px)",
-            "calc(90.5% - 80px)",
+            "calc(90.5% - 70px)",
+            "calc(90.5% - 90px)",
             "calc(90.5% - 105px)",
           ],
           right: "20.6%",
           position: "absolute",
         }}
+        onClick={() => selectionHandler("ROCK")}
       />
     </Box>
   );
