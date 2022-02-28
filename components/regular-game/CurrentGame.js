30479df0d5f21ef4fcb76a870b85321feba75cf4
Yago Batista <yagobatistafis@yahoo.com.br>
2022-02-28 03:00:01 -0300

    updating responsiviness in currentgame component

--- components/regular-game/CurrentGame.js
+++ components/regular-game/CurrentGame.js
@@ -16,39 +16,25 @@ const CurrentGame = () => {
   return (
     <Box
       bg="transparent"
-      w={["320px", "360px", "400px"]}
-      h={["280px", "300px", "340px"]}
+      w={["280px", "320px", "360px", "400px"]}
+      h={["240px", "280px", "300px", "340px"]}
       position="relative"
     >
-      <PlayButton
-        image={rockIcon}
-        config={{
-          top: "0",
-          right: "100%",
-          left: "0",
-          borderColor: "red.500",
-          position: "absolute",
-          h: ["140px", "150px", "170px"],
-          w: ["140px", "150px", "170px"],
-          borderWidth: ["15px", "17px", "20px"],
-        }}
-        onClick={() => selectionHandler("ROCK")}
-        alt="rock-image"
-      />
       <PlayButton
         image={paperIcon}
         config={{
           top: "0",
           right: "0",
           left: [
+            "calc(100% - 120px)",
             "calc(100% - 140px)",
             "calc(100% - 150px)",
             "calc(100% - 170px)",
           ],
           borderColor: "blue.600",
           position: "absolute",
-          h: ["140px", "150px", "170px"],
-          w: ["140px", "150px", "170px"],
+          h: ["120px", "140px", "150px", "170px"],
+          w: ["120px", "140px", "150px", "170px"],
           borderWidth: ["15px", "17px", "20px"],
         }}
         alt="paper-image"
@@ -58,6 +44,7 @@ const CurrentGame = () => {
         image={scissorsIcon}
         config={{
           top: [
+            "calc(100% - 120px)",
             "calc(100% - 140px)",
             "calc(100% - 150px)",
             "calc(100% - 170px)",
@@ -67,12 +54,27 @@ const CurrentGame = () => {
           borderColor: "yellow.500",
           margin: "auto",
           position: "absolute",
-          h: ["140px", "150px", "170px"],
-          w: ["140px", "150px", "170px"],
+          h: ["120px", "140px", "150px", "170px"],
+          w: ["120px", "140px", "150px", "170px"],
           borderWidth: ["15px", "17px", "20px"],
         }}
         alt="scissors-image"
         onClick={() => selectionHandler("SCISSORS")}
+      />
+      <PlayButton
+        image={rockIcon}
+        config={{
+          top: "0",
+          right: "100%",
+          left: "0",
+          borderColor: "red.500",
+          position: "absolute",
+          h: ["120px", "140px", "150px", "170px"],
+          w: ["120px", "140px", "150px", "170px"],
+          borderWidth: ["15px", "17px", "20px"],
+        }}
+        onClick={() => selectionHandler("ROCK")}
+        alt="rock-image"
       />
     </Box>
   );
